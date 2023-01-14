f8ef9660a71942c5f8ee04c5e1e55533d09a4164
mib <jajan151@seznam.cz>
2023-01-14 03:04:42 +0100

    input box added on top

--- web.py
+++ web.py
@@ -12,7 +12,11 @@ def add_todo():
 
 st.title("My todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase youre productivity.")
+st.write("This app is to increase youre <b>productivity</b>.",
+         unsafe_allow_html=True)
+
+st.text_input(label="", placeholder="Add a new todo...",
+              on_change=add_todo, key="new_todo")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -21,6 +25,3 @@ for index, todo in enumerate(todos):
         functions.write_todos(todos)
         del st.session_state[todo]
         st.experimental_rerun()
-
-st.text_input(label="", placeholder="Add a new todo...",
-              on_change=add_todo, key="new_todo")
